24ba602a6fab7dd96ceb6f04e8e74c54a8d15f4f
Merve Deniz <mervedenizsengul@hotmail.com>
2021-04-11 14:23:48 -0400

    added test case

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,5 +1,10 @@
 package tests;
 
-public class FirstTest {
+import org.testng.annotations.Test;
 
+public class FirstTest {
+    @Test
+    public void testCase1(){
+        System.out.println("Hello!");
+    }
 }
